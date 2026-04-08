8a9ceae8bd7bda9e4375b2c3366e52b80d175d7d
AyanXDeveloper <ayyanzaveri@gmail.com>
2026-04-08 23:27:18 +0500

    Applied Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Card from './components/Card'
+import { Route, Routes } from 'react-router-dom'
+import Gallery from './pages/Gallery'
 
 const App = () => {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Gallery />}  />
+      </Routes>
     </>
   )
 }

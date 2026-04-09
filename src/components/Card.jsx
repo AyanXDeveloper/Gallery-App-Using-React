6e6568eb3667e96092d9496c579839dd9e388023
AyanXDeveloper <ayyanzaveri@gmail.com>
2026-04-09 20:06:48 +0500

    Updated UI/UX

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <>
+      <div className='w-[300px] h-[400px] shadow-[#676767] shadow-[0_5px_25px] bg-[#333333] border-4 border-white flex items-center justify-center text-center p-5'>
+        <h1 className='text-2xl font-bold tracking-wide'>This is a card component</h1>
+      </div>
+    </>
   )
 }
 
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import Card from '../components/Card'
 
 const Gallery = () => {
-  return (
-    <>
-        <Card />
-    </>
-  )
+    return (
+        <>
+            <div className='bg-[#222222] text-white h-screen w-screen flex flex-col items-center justify-start gap-10 py-10'>
+                <div>
+                    <h1 className='font-bold tracking-wider text-[5rem] capitalize oswald'>Gallery App</h1>
+                </div>
+                <div>
+                    <Card />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Gallery
